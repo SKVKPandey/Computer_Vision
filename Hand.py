eafda007c1df9576724ec7a40eced0cef5bb67ca
Shashank Kashyap <50551759+SKVKPandey@users.noreply.github.com>
2022-04-09 16:26:18 +0530

    First Commit

---
+++ Hand.py
@@ -0,0 +1,44 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+pTime = 0
+cTime = 0
+cx = 0
+cy = 0
+
+while(True):
+	
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(img, "FPS: "+str(int(fps)), (18, 70), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+
+    cv2.imshow('Image', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+	    break
+
+
+cap.release()
+
+cv2.destroyAllWindows()
+
+
